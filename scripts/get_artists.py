32592568ed7b92bbb2e0693e247b8b2c46c45547
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-03 16:24:16 +0100

    initiliase get articles script

---
+++ scripts/get_artists.py
@@ -0,0 +1,6 @@
+def run(*args) :
+    # Get list of music genres pages from : "https://en.wikipedia.org/wiki/Lists_of_musicians"
+    # Get list af Artist from each page
+    # Search each artist or band in Deeze Api on : "https://api.deezer.com/search/artist?q={artist-name}"
+    # Create Artist object model 
+    print('get artists finished')
