09064d415f892e60e5e069ba5285725dc16e31e8
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-04 20:55:37 +0100

    Log artists data into log file

--- scripts/add_artists.py
+++ scripts/add_artists.py
@@ -1,14 +1,31 @@
-import requests
+import requests, queue
 from urllib.parse import quote
+from threading import Thread, Lock
 
 import json, os, time
 
+COUNT_OF_THREADS = 5
+global_lock = Lock()
+
 def log(content: dict) :
+    while global_lock.locked() :
+        continue
+    
+    global_lock.acquire()
     logging = list()
+
     if os.path.exists('logs.json') :
-        logging = json.loads(open('logs.json', 'r').read())
+        try :
+            logging = json.loads(open('logs.json', 'r').read())
+        except :
+            logging = list()
+
     logging.append(content)
-    open('logs.json', 'w').write(json.dumps(logging, indent=4))
+    
+    fp = open('logs.json', 'w')
+    fp.write(json.dumps(logging, indent=4))
+    fp.close()
+    global_lock.release()
 
 def getMostPopular(response: dict) :
     if response.get('data') is None or type(response.get('data')) != list or response.get('total') is None or response.get('total') <= 0 :
@@ -17,7 +34,6 @@ def getMostPopular(response: dict) :
         return None
 
     most_fan = list(sorted(response.get('data'), key=lambda item: item.get('nb_fan'), reverse=True))[0]
-
     return most_fan
 
 def getArtistsList() :
@@ -30,16 +46,41 @@ def searchForArtist(keyword) :
     content = json.loads(req.content.decode('utf-8'))
     artist_data = getMostPopular(content)
     if artist_data is not None :
-        log(artist_data)    
+        log(artist_data) 
     else :
         print(keyword, 'was no match')
+
+def StarProcessing(articles: list) :
+    start_process = time.time()
+    print('start processing shit')
+    threads = []
+    articles_queue = queue.Queue()
+    articles_queue.queue = queue.deque(articles)
+
+    def start_worker() :
+        print('start working')
+        while not articles_queue.empty() :
+            art = articles_queue.get()
+            searchForArtist(art)
+            index = len(articles) - articles_queue.qsize()
+            print(f'Artist fetched "{art}"; processed {index} in {time.time() - start_process} ; {round((index) / len(articles) * 100, 2)}% done')
+            time.sleep(1)
+
+    for _ in range(COUNT_OF_THREADS) :
+        t = Thread(target=start_worker)
+        threads.append(t)
+        t.start()
+
+    [thread.join() for thread in threads]
 
 def run() :
     start_time = time.time()
     artists_list = getArtistsList()
     
-    for i, art in enumerate(artists_list):
-        print(f'searching for {art} {i}/{len(artists_list)} ; {round(time.time() - start_time, 2)} passed')
-        searchForArtist(art)
+    # for i, art in enumerate(artists_list):
+    #     print(f'searching for {art} {i}/{len(artists_list)} ; {round(time.time() - start_time, 2)} passed')
+    #     searchForArtist(art)
+
+    StarProcessing(artists_list)
 
     print('add artists ended on', time.time() - start_time)
