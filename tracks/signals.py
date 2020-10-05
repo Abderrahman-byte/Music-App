233405d15c2741f0ffacd2f51d061ebfe9a359c4
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-05 16:36:21 +0100

    get albums of an artist

--- tracks/signals.py
+++ tracks/signals.py
@@ -1,9 +1,36 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Artist
-
 import requests, json
+from datetime import datetime
+
+from .models import Artist, Album
+
+def getAlbums(instance, nb) :
+    url = f'https://api.deezer.com/artist/{instance.deezer_id}'
+    req = requests.get(url)
+    content = json.loads(req.content.decode('utf-8'))
+
+    if content.get('data') is None or  type(content.get('data')) != list or len(content.get('data') <= 0) or content.get('total') == 0 :
+        return
+
+    for album in content.get('data') :
+        title = album.get('title')
+        deezer_id = album.get('id')
+        release = album.get('release_date')
+
+        if release is not None : release = datetime.strptime(release, '%Y-%m-%d')
+
+        try :
+            album = Album.objects.get(deezer_id=deezer_id)
+            print(f'album {title} with id {deezer_id} already exists')
+        except Album.DoesNotExist :
+            album = Album(deezer_id=deezer_id, title=title, release_date=release, artist=instance)
+
+        try :
+            album.save()
+        except Exception as ex :
+            print(ex) 
 
 def getArtistsDetails(instance) :
     url = f'https://api.deezer.com/artist/{instance.deezer_id}'
@@ -19,6 +46,8 @@ def getArtistsDetails(instance) :
     instance.save()
 
     nb_album = content.get('nb_album')
+
+    getAlbums(instance, nb)
 
 @receiver(post_save, sender=Artist)
 def AsignAlbums(sender, instance, created, *args, **kwargs) :
