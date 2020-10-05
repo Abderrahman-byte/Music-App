ab6efd67b2bba52a998a8116f0897df3a6f5bf49
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-05 15:51:27 +0100

    Get artist detail after being created

--- tracks/signals.py
+++ tracks/signals.py
@@ -3,7 +3,24 @@ from django.dispatch import receiver
 
 from .models import Artist
 
+import requests, json
+
+def getArtistsDetails(instance) :
+    url = f'https://api.deezer.com/artist/{instance.deezer_id}'
+
+    req = requests.get(url)
+    content = json.loads(req.content.decode('utf-8'))
+    
+    instance.picture = content.get('picture', instance.picture)
+    instance.picture_small = content.get('picture_small', instance.picture_small)
+    instance.picture_medium = content.get('picture_medium', instance.picture_medium)
+    instance.picture_big = content.get('picture_big', instance.picture_big)
+    instance.picture_xl = content.get('picture_xl', instance.picture_xl)
+    instance.save()
+
+    nb_album = content.get('nb_album')
+
 @receiver(post_save, sender=Artist)
 def AsignAlbums(sender, instance, created, *args, **kwargs) :
     if created :
-        print(f'{instance.id} {instance.name} justed created so you better asign albums')
+        getArtistsDetails(instance)
