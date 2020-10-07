8cc776e9e7ab036c080c0ba21fb7f0ea8dfaafed
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-07 22:19:21 +0100

    Add ArtistApiView music api urlpatterns

--- tracks/api/urls.py
+++ tracks/api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('tracks/top', views.TopTracks, name='top-tracks'),
     path('track/<str:id>', views.TrackApiView, name='track-details'),
-    path('album/<str:id>', views.AlbumApiView, name='track-details'),
+    path('album/<str:id>', views.AlbumApiView, name='album-details'),
+    path('artist/<str:id>', views.ArtistApiView, name='artist-details'),
 ]
