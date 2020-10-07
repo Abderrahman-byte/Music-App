978d7e9d9436c32778e96af4a093c70989746123
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-07 12:33:11 +0100

    Add id generator to id field

--- users/models.py
+++ users/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+import string
+from random import choice
+
+def generateUserId() :
+    allowed = string.ascii_letters + string.digits
+    return ''.join([choice(allowed) for _ in range(17)])
+
 class Account(AbstractUser):
-    pass
+    id = models.CharField(max_length=17, primary_key=True, default=generateUserId, editable=True)
+
+    # Add user email to be unique
+    # Profil_img
