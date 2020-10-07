4387f1c6a546b6c8d0535c6c4075e55f23ab6d50
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-07 16:48:32 +0100

    Create details album serializer

--- tracks/api/serializers.py
+++ tracks/api/serializers.py
@@ -33,22 +33,38 @@ class TrackSimpleSerializer(serializers.ModelSerializer) :
 class AlbumSimpleSerializer(serializers.ModelSerializer) :
     release_date = TimeStampField()
     genre = GenreSerializer(many=True, read_only=True)
+    nb_tracks = serializers.IntegerField(source='track_set.count', read_only=True)
 
     class Meta :
         model = Album
-        fields = ['id', 'title', 'release_date', 'cover_big', 'cover_medium', 'cover_small', 'cover_xl', 'genre']
+        fields = ['id', 'title', 'release_date', 'cover_big', 'cover_medium', 'cover_small', 'cover_xl', 'genre', 'nb_tracks']
 
 
 class ArtistSimpleSerializer(serializers.ModelSerializer) :
+    nb_album = serializers.IntegerField(source='album_set.count', read_only=True)
+
     class Meta :
         model = Artist
-        fields = ['id', 'name', 'picture', 'picture_small', 'picture_medium', 'picture_big', 'picture_xl']
+        fields = ['id', 'name', 'picture', 'picture_small', 'picture_medium', 'picture_big', 'picture_xl', 'nb_album']
 
 
 class TrackDetailedSerializer(serializers.ModelSerializer) :
     album = AlbumSimpleSerializer(read_only=True)
     artist = ArtistSimpleSerializer(source='album.artist', read_only=True)
+    release_date = TimeStampField()
 
     class Meta :
         model = Track
         fields = ['id', 'title', 'release_date', 'rank', 'preview', 'album', 'artist']
+
+class AlbumDetailedSerializer(serializers.ModelSerializer) :
+    release_date = TimeStampField()
+    genre = GenreSerializer(many=True, read_only=True)
+    nb_tracks = serializers.IntegerField(source='track_set.count', read_only=True)
+    tracks = TrackSimpleSerializer(source='track_set.all', read_only=True, many=True)
+    artist = ArtistSimpleSerializer(read_only=True)
+
+    class Meta :
+        model = Album
+        fields = ['id', 'title', 'release_date', 'cover_big', 'cover_medium', 'cover_small', 'cover_xl', 
+        'genre', 'nb_tracks', 'tracks', 'artist']
