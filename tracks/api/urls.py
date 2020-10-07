9eaa8ab6202249667b585a49667d01998a6e05c3
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-07 16:49:48 +0100

    Add AlbumApiView to urlpatterns

--- tracks/api/urls.py
+++ tracks/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tracks/top', views.TopTracks, name='get-top-tracks')
+    path('tracks/top', views.TopTracks, name='top-tracks'),
+    path('track/<str:id>', views.TrackApiView, name='track-details'),
+    path('album/<str:id>', views.AlbumApiView, name='track-details'),
 ]
