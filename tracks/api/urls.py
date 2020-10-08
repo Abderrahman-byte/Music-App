c054f27fd8bd463ff07d8fc6ffa3c8d210575c48
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-08 16:27:18 +0100

    Add artist albums

--- tracks/api/urls.py
+++ tracks/api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('album/<str:id>', views.AlbumApiView, name='album-details'),
     path('artist/<str:id>', views.ArtistApiView, name='artist-details'),
     path('artist/<str:id>/top', views.ArtistTopTracks, name='artist-top-tracks'),
+    path('artist/<str:id>/albums', views.ArtistAlbumApiView, name='artist-albums'),
 ]
