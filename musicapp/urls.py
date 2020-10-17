2e43a0e353a2a63789a2527aa10804c10dcfb385
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-17 15:52:35 +0100

    Add auth api url patterns into main url patterns

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/music/', include('tracks.api.urls'))
+    path('api/music/', include('tracks.api.urls')),
+    path('api/auth/', include('users.api.urls'))
 ]
