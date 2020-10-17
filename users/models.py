91a67bc213051bb45c8534392fbc21254892c713
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-17 17:53:43 +0100

    Change email field type to EmailField and verifie email before saving

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
-import string
+import string, re
 from random import choice
 
 def generateUserId() :
@@ -12,9 +13,17 @@ def generateUserId() :
 class Account(AbstractUser):
     id = models.CharField(max_length=17, primary_key=True, default=generateUserId, editable=True)
     is_active = models.BooleanField(default=False)
-    email = models.CharField(max_length=200, unique=True, null=False, error_messages={
+    email = models.EmailField(max_length=200, unique=True, blank=False, null=False, error_messages={
         'unique': _('A user with that email already exists.')
     })
 
     # Profil_img
     # added playlists
+
+    def save(self, *args, **kwargs) :
+        EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+        if not EMAIL_REGEX.match(self.email) :
+            raise ValidationError(_('Email address is not valid'), code='invalid')
+
+        super().save(*args, **kwargs)
