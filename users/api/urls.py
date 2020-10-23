557fa332df7b1747e170ef88e5a5df5735fee9a5
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-23 18:50:27 +0100

    Add Register Api View to auth urlpatterns

--- users/api/urls.py
+++ users/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.LoginView, name='login')
+    path('login', views.LoginView, name='login'),
+    path('register', views.RegisterView, name='register'),
 ]
