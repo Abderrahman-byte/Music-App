633c32f2e63cce0a7c37e48037c06dafb85b4455
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-24 18:35:18 +0100

    Send activation mail

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,10 @@ from django.dispatch import receiver
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.urls import reverse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+import logging
 
 from .tokens import activate_accounts_token
 from .models import Account
@@ -11,6 +15,19 @@ def sendActivationEmail(instance) :
     uidb64 = urlsafe_base64_encode(force_bytes(instance.id))
     token = activate_accounts_token.make_token(instance)
     path = reverse('activate-account', args=(uidb64, token))
+
+    try :
+        message = render_to_string('users/activate_mail.html', {'path': path, 'user': instance})
+        send_mail(
+            subject = 'Activation of your account on Music App', 
+            message = message,
+            from_email='support@musicapp.ma' , 
+            recipient_list = ['to@yourbestuser.com'], 
+            fail_silently = False
+        )
+    except Exception as ex :
+        print(ex)
+        logging.getLogger('errors').error(f'Exception from sendActivationEmail : {ex.__str__()}')        
     print(f'you must send a activation message to {instance.pk} user. url is {path}')
 
 @receiver(post_save, sender=Account)
