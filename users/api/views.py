37ed85c76f5e130f263835212c94ca73d1b6df67
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-24 16:07:48 +0100

    Create activation account view and fix bug in registration view

--- users/api/views.py
+++ users/api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
 
 from .serializers import AccountSerializer
+from ..models import Account
+from ..tokens import activate_accounts_token
 
 @api_view(['POST'])
 def LoginView(request) :
@@ -32,28 +36,39 @@ def LoginView(request) :
 @api_view(['POST'])
 def RegisterView(request) :
     data = request.data
+    is_there_error = False
+    required_fields = ('username', 'password', 'email', 'first_name', 'last_name')
+
     if data.get('username') is None : 
+        is_there_error = True
         data_resp = {'detail': 'Username field is required'}
         status = 400
     
     elif data.get('first_name') is None : 
+        is_there_error = True
         data_resp = {'detail': 'First name field is required'}
         status = 400
     
     elif data.get('last_name') is None : 
+        is_there_error = True
         data_resp = {'detail': 'Last name field is required'}
         status = 400
 
     elif data.get('email') is None : 
+        is_there_error = True
         data_resp = {'detail': 'Email field is required'}
         status = 400
 
     elif data.get('password') is None : 
+        is_there_error = True
         data_resp = {'detail': 'passwrd field is required'}
         status = 400
     
+    if is_there_error : return Response(data_resp, status=status, content_type='application/json')
+    validated_data = dict([(field, data.get(field)) for field in required_fields])
+
     try :
-        account =  AccountSerializer().create(**data)
+        account =  AccountSerializer().create(**validated_data)
         data_resp = {'success': 'User account is created. you check your email inbox to verifie your account.'}
         status = 201
     except Exception as ex :
@@ -61,3 +76,26 @@ def RegisterView(request) :
         status = 400
 
     return Response(data_resp, status=status, content_type='application/json')
+
+
+@api_view(['GET'])
+def ActivateAccount(request, uidb64, token) :
+    user_id = force_text(urlsafe_base64_decode(uidb64))
+
+    try :
+        account = Account.objects.get(pk=user_id)
+    except Account.DoesNotExist :
+        return Response({'detail': 'Activation link is invalid!'}, status=400, content_type='application/json')
+
+    if not activate_accounts_token.check_token(account, token) :
+        return Response({'detail': 'Activation link is invalid!'}, status=400, content_type='application/json')
+
+    if account.is_active : 
+        return Response({'success': 'Account is already activated'}, status=200, content_type='application/json')
+
+    account.is_active = True
+    account.save()
+    login(request, account)
+    serializer = AccountSerializer(account)
+
+    return Response({'data': serializer.data, 'success': 'User account has been activated'}, content_type='application/json')
