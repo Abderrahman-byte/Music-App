443e2784cddabb49cd6299e4baeeaad437231af0
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-25 16:18:15 +0100

    Create Favorite Lists Models

--- playlists/models.py
+++ playlists/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from users.models import generateUserId, Account
-from tracks.models import Track, Artist
+from tracks.models import Track, Artist, Album
 
 class TracksPlaylist(models.Model) :
     id = models.CharField(max_length=17, primary_key=True, default=generateUserId, editable=False)
@@ -27,3 +27,22 @@ class Follow(models.Model) :
         constraints = [
             models.UniqueConstraint(fields=['user', 'artist'], name='user_follow_artist_only_once'),
         ]
+
+class FavoriteList(models.Model) :
+    user = models.OneToOneField(Account, on_delete=models.CASCADE, primary_key=True, editable=False)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta :
+        abstract = True
+
+class FavoriteTracksList(FavoriteList) :
+    tracks = models.ManyToManyField(Track)
+    
+class FavoriteAlbumsList(FavoriteList) :
+    albums = models.ManyToManyField(Album)
+
+class FavoriteArtistsList(FavoriteList) :
+    artists = models.ManyToManyField(Artist)
+
+class FavoritePlaylistsList(FavoriteList) :
+    playlist = models.ManyToManyField(TracksPlaylist)
