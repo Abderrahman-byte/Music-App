4c9b9d1e591c95f09b82705cb0e1ac66f2ba8a20
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-25 16:02:27 +0100

    Create follow model

--- playlists/models.py
+++ playlists/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from users.models import generateUserId, Account
-from tracks.models import Track
+from tracks.models import Track, Artist
 
 class TracksPlaylist(models.Model) :
     id = models.CharField(max_length=17, primary_key=True, default=generateUserId, editable=False)
@@ -16,4 +16,14 @@ class TracksPlaylist(models.Model) :
     class Meta :
         constraints = [
             models.UniqueConstraint(fields=['author', 'title'], name='unique-title-playlist-for-creator'),
+        ]
+
+class Follow(models.Model) :
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'artist'], name='user_follow_artist_only_once'),
         ]
