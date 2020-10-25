09e0b97cf78ca271b8d5d4867730cad63f76fdbc
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-25 15:31:20 +0100

    Create TracksPlaylist model

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import generateUserId, Account
+from tracks.models import Track
+
+class TracksPlaylist(models.Model) :
+    id = models.CharField(max_length=17, primary_key=True, default=generateUserId, editable=False)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Account, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    tracks = models.ManyToManyField(Track)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(fields=['author', 'title'], name='unique-title-playlist-for-creator'),
+        ]
