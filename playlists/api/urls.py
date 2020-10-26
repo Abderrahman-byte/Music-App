168878fd9bca5c3466a39a412a7e6b341cd915ff
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-26 16:35:16 +0100

    Add FavoriteTracksApi to url patterns

--- playlists/api/urls.py
+++ playlists/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.UserPlaylists.as_view(), name='user-playlists'),
     path('list/<str:id>', views.PlaylistDetails.as_view(), name='playlist-details'),
     path('subscription/<str:id>', views.Subscription.as_view(), name='subscription'),
+    path('favorite/tracks', views.FavoriteTracksAPI.as_view(), name='favorite-tracks'),
 ]
