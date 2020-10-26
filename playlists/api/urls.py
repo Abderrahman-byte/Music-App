e3c8b2401ff4e9fbb2be46ade345c84d4938dedd
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-26 11:31:46 +0100

    Add PlaylistDetails to urlpatterns

--- playlists/api/urls.py
+++ playlists/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserPlaylists.as_view(), name='user-playlists')
+    path('', views.UserPlaylists.as_view(), name='user-playlists'),
+    path('list/<str:id>', views.PlaylistDetails.as_view(), name='playlist-details'),
 ]
