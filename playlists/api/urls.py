0719dedcb39c29e0696fc2cce0fdbcc42341d3c6
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-26 19:35:51 +0100

    Separate favorite view

--- playlists/api/urls.py
+++ playlists/api/urls.py
@@ -1,13 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
+
+favorite_lists_urlpatterns = [
+    path('tracks', views.FavoriteTracksAPI.as_view(), name='favorite-tracks'),
+    path('artists', views.FavoriteArtistsAPI.as_view(), name='favorite-artists'),
+    path('albums', views.FavoriteAlbumsAPI.as_view(), name='favorite-albums'),
+    path('playlists', views.FavoritePlaylistsAPI.as_view(), name='favorite-playlists'),
+]
 
 urlpatterns = [
     path('', views.UserPlaylists.as_view(), name='user-playlists'),
     path('list/<str:id>', views.PlaylistDetails.as_view(), name='playlist-details'),
     path('subscription/<str:id>', views.Subscription.as_view(), name='subscription'),
-    path('favorite/tracks', views.FavoriteTracksAPI.as_view(), name='favorite-tracks'),
-    path('favorite/artists', views.FavoriteArtistsAPI.as_view(), name='favorite-artists'),
-    path('favorite/albums', views.FavoriteAlbumsAPI.as_view(), name='favorite-albums'),
-    path('favorite/playlists', views.FavoritePlaylistsAPI.as_view(), name='favorite-playlists'),
+    path('favorite/', include(favorite_lists_urlpatterns))
 ]
