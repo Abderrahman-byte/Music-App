57660671436ef65885cf9aad1728fc11147fb73f
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-26 15:47:34 +0100

    Add Subscription ApiView to playlists api url patterns

--- playlists/api/urls.py
+++ playlists/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.UserPlaylists.as_view(), name='user-playlists'),
     path('list/<str:id>', views.PlaylistDetails.as_view(), name='playlist-details'),
+    path('subscription/<str:id>', views.Subscription.as_view(), name='subscription'),
 ]
