f1442129628ba9e3c5ad1b97011a4cf1e541b37a
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-26 19:31:50 +0100

    Add favorite view to playlists api urlpatterns

--- playlists/api/urls.py
+++ playlists/api/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('list/<str:id>', views.PlaylistDetails.as_view(), name='playlist-details'),
     path('subscription/<str:id>', views.Subscription.as_view(), name='subscription'),
     path('favorite/tracks', views.FavoriteTracksAPI.as_view(), name='favorite-tracks'),
+    path('favorite/artists', views.FavoriteArtistsAPI.as_view(), name='favorite-artists'),
+    path('favorite/albums', views.FavoriteAlbumsAPI.as_view(), name='favorite-albums'),
+    path('favorite/playlists', views.FavoritePlaylistsAPI.as_view(), name='favorite-playlists'),
 ]
