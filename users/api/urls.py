06d5e028bdf4025850e9ce8e0cab98676123eed9
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-28 12:40:19 +0100

    Add AccountDetails to auth api urlpatterns

--- users/api/urls.py
+++ users/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', views.LoginView, name='login'),
     path('register', views.RegisterView, name='register'),
     path('activate/<uidb64>/<token>', views.ActivateAccount, name='activate-account'),
+    path('account', views.AccountDetails.as_view(), name='account-details'),
 ]
