50d860884139721567c46a830d91920701cf8a83
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-28 18:17:46 +0100

    Add Search view api to music urlpatterns

--- tracks/api/urls.py
+++ tracks/api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('artist/<str:id>', views.ArtistApiView, name='artist-details'),
     path('artist/<str:id>/top', views.ArtistTopTracks, name='artist-top-tracks'),
     path('artist/<str:id>/albums', views.ArtistAlbumApiView, name='artist-albums'),
+    path('search', views.Search, name='search'),
 ]
