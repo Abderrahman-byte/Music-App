80f717903514643d22e25040ad5476e006a24db1
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-30 13:17:56 +0100

    Init authenticate function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,28 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
 const AuthContext = createContext({})
 
 export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(undefined)
+
+    const authenticate = async () => {
+        const req = await fetch(`${process.env.API_URL}/api/auth/account`)
+
+        if(req.status >= 200 && req.status < 300) {
+            console.log(await req.json())
+        } else {
+            console.log(await req.text())
+            setUser(null)
+        }
+    }
+
+    useEffect(() => {
+        authenticate()
+    }, [])
+
     return (
         <AuthContext.Provider value={{}}>
             {children}
         </AuthContext.Provider>
     )
 }
-
-export const UseAuthContext = useContext(AuthContext)
