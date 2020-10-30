a60f5b719794ddacf6d9a0ea77e5189edb8077e6
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-30 13:19:03 +0100

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
 
 class App extends React.Component {
-    state = {
-        title: 'Abderrahmane'
-    }
-
     render = () => {
-        console.log('we good')
         return (
-            <div className={styles.App}>
-                <h1>{this.state.title}</h1>
-            </div>
+            <div className='App'></div>
         )
     }
 }
