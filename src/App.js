ec03e99531a126a146cb38b2a18868cf661f45db
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-10-31 10:37:27 +0100

    Create blank nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import './styles/App.scss'
+
 import { HeaderBar } from './components/HeaderBar'
+import { NavBar } from './components/NavBar'
 
 const App = () => {
     return (
         <div className='App'>
             <HeaderBar />
+            <div className='content'>
+                <NavBar />
+            </div>
         </div>
     )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../styles/NavBar.scss'
+
+export const NavBar = () => {
+    return (
+        <nav className='NavBar' role='navigation'>
+            this nav
+        </nav>
+    )
+}
