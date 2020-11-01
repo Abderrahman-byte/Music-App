7fff38f9281996ee74ea9179d4c621a2a250d8f0
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-01 11:17:17 +0100

    Change loading model location

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
-import { LoadingModel } from './LoadingModel'
+
+import { LoadingModel } from '../components/LoadingModel'
 import { ModelsContext } from './ModelsContext'
 
 const AuthContext = createContext({})
