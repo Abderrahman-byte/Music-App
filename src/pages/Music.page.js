1ecb371465e8c5fe0c2d0510f4a25f12b3267984
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-01 10:55:04 +0100

    Fetch top tracks from server

--- src/pages/Music.page.js
+++ src/pages/Music.page.js
@@ -1,10 +1,48 @@
 import React from 'react'
 
+import { PageHeader } from '../components/PageHeader'
+
 export class MusicPage extends React.Component {
+    state = {
+        data: [],
+        currentPage: 1,
+        itemsPerPage: 24,
+        isLoading: true,
+        nextRequestUrl: null
+    }
+
+    componentDidMount = () => {
+        this.fetchMusicData()   
+    }
+
+    fetchMusicData = async () => {
+        this.setState({...this.state, isLoading: true})
+        let response = {data: []}
+        const index = (this.state.currentPage - 1) * this.state.itemsPerPage
+        const url = this.state.nextRequestUrl || `${process.env.API_URL}/api/music/tracks/top?index=${index}&limit=${this.state.itemsPerPage}`
+        const req = await fetch(url)
+
+        if(req.status >= 200 && req.status < 300) {
+            response = await req.json()
+        } else {
+            console.error(await req.json())
+        }
+
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                isLoading: false,
+                currentPage: prevState.currentPage + 1,
+                nextRequestUrl: response.next || null,
+                data : [...prevState.data, ...response.data]
+            }   
+        })
+    }
+
     render = () => {
         return (
-            <div className='MusicPage'>
-                
+            <div className='MusicPage page'>
+                <PageHeader title='Explore The Music' />
             </div>
         )
     }
