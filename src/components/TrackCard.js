52b5f0b536ee158f682236ce0278c1cfeaba7c57
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-01 16:59:47 +0100

    Play and pause music on click on the playbtn

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import '../styles/TrackCard.scss'
 
+import { MusicPlayer } from '../context/MusicPlayer'
+
 export const TrackCard = ({data}) => {
+    const { play, currentId, isPlaying, setPlayingStatus} = useContext(MusicPlayer)
+
+    const playTrack = () => {
+        if(!isPlaying && currentId === data.id) {
+            setPlayingStatus(true)
+        } else {
+            play([data])
+        }
+    }
+
+    const handlePlaybtnClicked = () => {
+        if(isPlaying && currentId === data.id) {
+            setPlayingStatus(false)
+        } else {
+            playTrack()
+        }
+    }
+
     return (
         <div className='TrackCard'>
             <div className='play-div'>
                 <img className='cover' src={data.album.cover_medium} alt={data.album.title} />
-                <button className='play'>
+                <button onClick={handlePlaybtnClicked} className='play'>
                     <i className="fas fa-play"></i>
                 </button>
             </div>
