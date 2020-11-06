56bd9d588a9c65fb52d6d766b523b9bdfe5e3513
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-06 10:35:26 +0100

    Create basic playlist table

---
+++ src/components/PlaylistTable.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import '../styles/PlaylistTable.scss'
+import { TrackRow } from './TrackRow';
+
+export const PlaylistTable = ({items, withAlbum, withArtist}) => {
+    return (
+        <table className='PlaylistTable'>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th></th>
+                    <th className='text-start'>title</th>
+                    {withAlbum ? (<th>Album</th>): (null)}
+                    {withArtist ? (<th>Artist</th>): (null)}
+                    <th className='btn-col'>Favorite</th>
+                    <th className='btn-col'>Add To Playlist</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {items.map((track, i) => <TrackRow 
+                    key={track.id} 
+                    data={track} 
+                    index={i + 1} 
+                    withAlbum={withAlbum || false}
+                    withArtist={withArtist || false}
+                />)}
+            </tbody>
+        </table>
+    )
+}
+
+PlaylistTable.propTypes = {
+    items : PropTypes.array.isRequired,
+    withAlbum: PropTypes.bool,
+    withArtist: PropTypes.bool,
+}
---
+++ src/components/TrackRow.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import '../styles/TrackRow.scss'
+
+export const TrackRow = ({data, index, withAlbum, withArtist}) => {
+    console.log(data)
+    return (
+        <tr className='TrackRow'>
+            <td>{index}</td>
+            <td></td>
+            <td className='text-start'>{data.title}</td>
+            {withAlbum ? <td>{data.album?.title} </td> : null}
+            {withArtist ? <td>{data.artist?.name} </td> : null}
+            <td className='btn-col'></td>
+            <td className='btn-col'></td>
+        </tr>
+    )
+}
+
+TrackRow.propTypes = {
+    data: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    withAlbum: PropTypes.bool,
+    withArtist: PropTypes.bool,
+}
