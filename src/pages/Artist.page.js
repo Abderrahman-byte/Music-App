e72e3db41e0207d11a79592cf62316d8ea3f6b90
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-06 20:07:12 +0100

    Fetch artist data

--- src/pages/Artist.page.js
+++ src/pages/Artist.page.js
@@ -1,7 +1,44 @@
 import React from 'react'
 
+import { ModelsContext } from '../context/ModelsContext'
+import { LoadingModel } from '../components/LoadingModel'
+
 export class ArtistPage extends React.Component {
-    
+    static contextType = ModelsContext
+
+    state = {
+        data: null,
+        topTracks: {},
+        isLoading: true,
+        error: false
+    }
+
+    componentDidMount = () => {
+        this.fetchArtistData()
+    }
+
+    fetchArtistData = async () => {
+        const { openModel, closeModel } = this.context
+        openModel(<LoadingModel msg='Loading Album data' />, false)
+
+        const id = this.props.match?.params?.id
+        const req = await fetch(`${process.env.API_URL}/api/music/artist/${id}`)
+        
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            const topTracksData = {...data.top}
+            delete(data.top)
+
+            this.setState({data: {...data}, topTracks: {...topTracksData}, isLoading: false, error: false})
+        } else {
+            const data = await req.json()
+            console.error(data)
+            this.setState({...this.state, error: true, isLoading: false})
+        }
+
+        closeModel()
+    }
+
     render = () => {
         return (
             <div className='ArtistPage page'>
