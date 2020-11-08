65fb1745044f103edf0a953669697b796065bf04
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-08 18:34:32 +0100

    Create Album Card component

---
+++ src/components/AlbumCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../styles/AlbumCard.scss'
+
+export const AlbumCard = ({data}) => {
+    console.log(data)
+    return (
+        <div className='AlbumCard'>
+
+        </div>
+    )
+}
