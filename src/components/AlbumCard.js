2143c103d63b3609503ff23d860b56e6cca48ff3
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-09 19:41:09 +0100

    Fix Margin and size

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -4,10 +4,9 @@ import PropTypes from 'prop-types'
 
 import '../styles/AlbumCard.scss'
 
-export const AlbumCard = ({data}) => {
-    console.log(data)
+export const AlbumCard = ({data, itemsPerLine}) => {
     return (
-        <div className='AlbumCard'>
+        <div className={`AlbumCard perline-${itemsPerLine}`}>
             <div className='cover-container'>
                 <img className='cover' src={data.cover_medium} alt={data.title} />
                 <div className='front-drop'>
@@ -27,5 +26,6 @@ AlbumCard.propTypes = {
         id: PropTypes.string.isRequired,
         cover_medium: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired
-    }).isRequired
+    }).isRequired,
+    itemsPerLine: PropTypes.number.isRequired
 }
