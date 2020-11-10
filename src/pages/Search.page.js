9c278b88ec3baee523ba350071813382c67e24f0
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-10 20:14:58 +0100

    Create search page

---
+++ src/pages/Search.page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class SearchPage extends React.Component {
+    // componentDidMount = () => {
+    //     console
+    // }
+    
+    render = () => {
+        return (
+            <div className='SearchPage page'></div>
+        )
+    }
+}
