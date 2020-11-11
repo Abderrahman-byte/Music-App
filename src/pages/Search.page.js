8b5993731b496a471694afd3b07ac81c1c551cef
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-11 20:53:52 +0100

    Fetch search data

--- src/pages/Search.page.js
+++ src/pages/Search.page.js
@@ -1,14 +1,42 @@
 import React from 'react'
 
 import { parseQuery } from '../utils/generic'
+import { ModelsContext } from '../context/ModelsContext'
+import { LoadingModel } from '../components/LoadingModel'
 
 export class SearchPage extends React.Component {
+    static contextType = ModelsContext
+
     state = {
-        query: null
+        query: null,
+        isLoading: true,
+        error: false
     }
 
     componentDidMount = () => {
         this.getQuery()
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        if(!prevState.query && this.state.query !== null && this.state.query !== undefined) this.getSearchData()
+    }
+
+    getSearchData = async () => {
+        if(this.state.error || !this.state.query) return
+        const { openModel, closeModel } = this.context
+        openModel(<LoadingModel msg='Sync search data' />)
+
+        const req = await fetch(`${process.env.API_URL}/api/music/search?query=${this.state.query}`)
+        
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            console.log(data)
+        } else {
+            console.error(await req.json())
+            this.setState({error: false})
+        }
+
+        closeModel()
     }
 
     getQuery = () => {
@@ -16,9 +44,12 @@ export class SearchPage extends React.Component {
         const parsed = parseQuery(queryString)
         let query
 
-        if(parsed.query === null || parsed.query === undefined) {
-            throw Error('parsed query is null or undefined')
-        } else if(Array.isArray(parsed.query)) {
+        if(parsed.query === null || parsed.query === undefined || parsed.query === '') {
+            this.setState({query: null, isLoading: false, error: true})
+            return
+        } 
+        
+        if(Array.isArray(parsed.query)) {
             query = parsed.query[0]
         } else {
             query = parsed.query
