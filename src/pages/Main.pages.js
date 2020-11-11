93b0a596f54954bd21b4950a0faa5dacc1ca40de
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-11 19:40:43 +0100

    Add Search page to main pages

--- src/pages/Main.pages.js
+++ src/pages/Main.pages.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import { AlbumPage } from './Album.page'
 import { ArtistPage } from './Artist.page'
 import { MusicPage } from './Music.page'
+import { SearchPage } from './Search.page'
 
 export const MainPages = () => {
     return (
@@ -11,6 +12,7 @@ export const MainPages = () => {
             <Route exact path='/' component={MusicPage} />
             <Route exact path='/album/:id' component={AlbumPage} />
             <Route exact path='/artist/:id' component={ArtistPage} />
+            <Route exact path='/search' component={SearchPage} />
         </Switch>
     )
 }
