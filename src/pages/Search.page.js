2e6a30e433c8c74cabc114445a518382d8dae9d6
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-11 19:41:14 +0100

    Get query from location

--- src/pages/Search.page.js
+++ src/pages/Search.page.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import { parseQuery } from '../utils/generic'
+
 export class SearchPage extends React.Component {
-    // componentDidMount = () => {
-    //     console
-    // }
+    state = {
+        query: null
+    }
+
+    componentDidMount = () => {
+        this.getQuery()
+    }
+
+    getQuery = () => {
+        const queryString = this.props.location?.search
+        const parsed = parseQuery(queryString)
+        let query
+
+        if(parsed.query === null || parsed.query === undefined) {
+            throw Error('parsed query is null or undefined')
+        } else if(Array.isArray(parsed.query)) {
+            query = parsed.query[0]
+        } else {
+            query = parsed.query
+        }
+
+        this.setState({query})
+    }
     
     render = () => {
         return (
