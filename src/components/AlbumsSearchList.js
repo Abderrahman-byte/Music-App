c954d684891527a978dc097b45db557dc050fef6
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-13 15:23:12 +0100

    Create Albums search list manager component

---
+++ src/components/AlbumsSearchList.js
@@ -0,0 +1,66 @@
+import React, { createRef } from 'react'
+import PropTypes from 'prop-types'
+
+import { AlbumCard } from './AlbumCard'
+
+export class AlbumsSearchList extends React.Component {
+    state = {
+        data: [...this.props.data],
+        currentPage: 1,
+        itemsPerPage: 5,
+        containerRef: createRef(),
+        itemsPerLine: 5,
+        total: undefined
+    }
+
+    componentDidMount = () => {
+        this.getItemsPerLine()
+        window.addEventListener('resize', this.getItemsPerLine)
+    }
+    
+    componentWillUnmount = () => {
+        window.removeEventListener('resize', this.getItemsPerLine)
+    }
+
+    getItemsPerLine = () => {
+        const container = this.state.containerRef.current
+        const itemsPerLine = Math.floor(parseFloat(getComputedStyle(container).width) / 170)
+        this.setState({itemsPerLine: itemsPerLine})
+    }
+
+    fetchNextAlbumsData = async () => {
+        const nextPage = this.state.currentPage + 1
+        const index = (nextPage * this.state.itemsPerPage) - this.state.itemsPerPage
+        const url = `api/music/search/albums?query=${this.props.query}&limit=${this.state.itemsPerPage}&index=${index}`
+        const req = await fetch(`${process.env.API_URL}/${url}`)
+
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            this.setState(prevState => {
+                return {
+                    data: [...prevState.data, ...data.data],
+                    total: data.total,
+                    currentPage: nextPage
+                }
+            })
+        } else {
+            console.error(await req.json())
+        }
+    }
+
+    render = () => {
+        return (
+            <div className='ArtistAlbums' ref={this.state.containerRef}>
+                <div className='albums-container'>
+                    {this.state.data.map(item => <AlbumCard itemsPerLine={this.state.itemsPerLine} key={item.id} data={item} />)}
+                    <button onClick={this.fetchNextAlbumsData} className='add-more'>Load More</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+AlbumsSearchList.propTypes = {
+    data: PropTypes.array.isRequired,
+    query: PropTypes.string.isRequired
+}
