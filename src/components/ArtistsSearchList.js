67453ad9bcc2953d85a443114ad7ae49e14cc469
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-13 17:54:21 +0100

    Load more artists

--- src/components/ArtistsSearchList.js
+++ src/components/ArtistsSearchList.js
@@ -4,14 +4,19 @@ import PropTypes from 'prop-types'
 import '../styles/ArtistsSearchList.scss'
 
 import { ArtistCard } from './ArtistCard'
+import { ModelsContext } from '../context/ModelsContext'
+import { LoadingModel } from './LoadingModel'
 
 export class ArtistsSearchList extends React.Component {
+    static contextType = ModelsContext
+
     state = {
         data: [...this.props.data],
         currentPage: 1,
         itemsPerPage: 5,
         itemsPerLine: 1,
-        containerRef: createRef()
+        containerRef: createRef(),
+        total: undefined
     }
 
     componentDidMount = () => {
@@ -29,22 +34,52 @@ export class ArtistsSearchList extends React.Component {
         this.setState({itemsPerLine: itemsPerLine})
     }
 
+    fetchNextArtistsData = async () => {
+        const { openModel, closeModel } = this.context
+        openModel(<LoadingModel msg='Loading More Artists' />)
+        const nextPage = this.state.currentPage + 1
+        const index = (nextPage * this.state.itemsPerPage) - this.state.itemsPerPage
+        const url = `api/music/search/artists?query=${this.props.query}&limit=${this.state.itemsPerPage}&index=${index}`
+        const req = await fetch(`${process.env.API_URL}/${url}`)
+
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            
+            this.setState(prevState => {
+                return {
+                    data: [...prevState.data, ...data.data],
+                    currentPage: nextPage,
+                    total: data.total
+                }
+            })
+        } else {
+            console.error(await req.json())
+        }
+        closeModel()
+    }
+
     render = () => {
         return (
             <div className='ArtistsSearchList' ref={this.state.containerRef}>
                 <h6 className='title'>Artists</h6>
                 <div className='cards-container'>
-                    {this.props.data.map(artist => <ArtistCard 
+                    {this.state.data.map(artist => <ArtistCard 
                         key={artist.id} 
                         data={artist} 
                         itemsPerLine={this.state.itemsPerLine} 
                     />)}
                 </div>
+
+                {(this.state.total ? this.state.data.length < this.state.total : true) ? (
+                    <button onClick={this.fetchNextArtistsData} className='add-more'>Load More</button>
+                ): null }
             </div>
         )
     }
 }
 
 ArtistsSearchList.propTypes = {
-    data: PropTypes.array.isRequired
+    data: PropTypes.array.isRequired,
+    query: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired
 }
