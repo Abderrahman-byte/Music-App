d2bfc2ce1e6f993012d8433d0d9760155e3cabb1
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-19 10:50:05 +0100

    For allowing only unauthenticated user

---
+++ src/components/UnAuthenticatedOnly.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { Redirect } from 'react-router-dom'
+import { ClassWithMultipleContexts } from '../components/ClassWithMultipleContexts'
+import { AuthContext } from '../context/AuthContext'
+import { ModelsContext } from '../context/ModelsContext'
+
+export class UnAuthenticatedOnly extends React.Component {
+    state = {
+        isAuthenticated: false
+    }
+
+    componentDidMount = () => {
+        this.checkAuthentication()
+    }
+
+    checkAuthentication = () => {
+        if(this.context.AuthContext.isLoading) {
+            setTimeout(this.checkAuthentication, 500)
+        } else {
+            if(this.context.AuthContext?.user && this.context.AuthContext?.user?.id) {
+                this.setState({ isAuthenticated: true })
+            }
+            this.context.ModelsContext.closeModel()
+        }
+    }
+
+    render = () => {
+        if(this.state.isAuthenticated) {
+            return <Redirect to={this.props.redirectTo || '/'}></Redirect>
+        } else {
+            return (
+                <>{this.props.children}</>
+            )
+        }
+    }
+}
+
+export default ClassWithMultipleContexts(UnAuthenticatedOnly, {AuthContext, ModelsContext})
