6fab2b0b3a3de6b020ca0539260e3dfb3093e24d
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-20 18:00:15 +0100

    verifie form data

--- src/components/LoginFormManager.js
+++ src/components/LoginFormManager.js
@@ -14,8 +14,36 @@ export class LoginFormManager extends React.Component {
         this.setState({ data: {...newData}})
     }
 
+    checkData = () => {
+        const data = {...this.state.data}
+
+        if(data.username === '') {
+            console.error('Username field is required')
+            return false
+        }
+
+        if(data.password === '') {
+            console.error('Password field is required')
+            return false
+        }
+
+        if(data.username.length < 6) {
+            console.error('Username field length is not enough.')
+            return false
+        }
+
+        if(data.password.length < 6) {
+            console.error('Password field length is not enough.')
+            return false
+        }
+
+        return true
+    }
+
     submitHandler = (e) => {
         e.preventDefault()
+        const dataIsValidated = this.checkData()
+        
     }
 
     render = () => (<LoginForm 
