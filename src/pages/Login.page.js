de206bd8ce07379d0d2da277bb766b06e55326f0
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-25 20:14:42 +0100

    Pass history object to Login manager

--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 import '../styles/LoginPage.scss'
 
 import LoginFormManager from '../components/LoginFormManager'
 
 export const LoginPage = () => {
+    const history = useHistory()
+    
     return (
         <div className='LoginPage page'>
-            <LoginFormManager />
+            <LoginFormManager history={history} />
         </div>
     )
 }
