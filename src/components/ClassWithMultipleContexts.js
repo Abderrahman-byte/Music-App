71e19b0626a95096dde7c283ac6446121cf676d8
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-25 19:40:17 +0100

    Delete class with multiple contexts provided

--- src/components/ClassWithMultipleContexts.js
+++ src/components/ClassWithMultipleContexts.js
@@ -1,24 +1,4 @@
-import React, { createContext, useContext, useState } from 'react'
-
-export const NestedProviders = ({Provider, others, children}) => {
-    const [NextProvider, setNextProvider] = useState(() => {
-        return others[0] || null
-    })
-
-    const [otherProviders, setOtherProviders] = useState(() => {
-        return others.length > 1 ? others.slice(1) : [] 
-    })
-
-    return (
-        <Provider>
-            {NextProvider ? (
-                <NextProvider otherProviders={otherProviders}>
-                    {children}
-                </NextProvider>
-            ) : ({children})}
-        </Provider>
-    )
-}
+import React, { createContext, useContext } from 'react'
 
 export const ClassWithMultipleContexts = (Component, contexts) => {   
     const MultipleContexts = createContext(null)
@@ -35,39 +15,6 @@ export const ClassWithMultipleContexts = (Component, contexts) => {
             <MultipleContexts.Provider value={contextMap}>
                 <Component {...props} />
             </MultipleContexts.Provider>
-        )
-    }
-
-    return MultipleContextsProvider
-}
-
-export const ClassWithMultipleContextsProvided = (Component, contexts) => {   
-    const MultipleContexts = createContext(null)
-    Component.contextType = MultipleContexts
-
-    const MultipleContextsProvider = (props) => {
-        const contextMap = {}
-
-        contexts.forEach(contextObj => {
-            contextMap[contextObj.context.displayName] = contextObj.context
-        })
-
-        return (
-            <MultipleContexts.Provider value={contextMap}>
-                <Component {...props} />
-            </MultipleContexts.Provider>
-        )
-    }
-
-    return (props) => {
-        const [providers, setProviders] = useState(() => {
-            return contexts.map(contextObj => contextObj.provider)
-        })
-
-        return (
-            <NestedProviders Provider={providers[0]} others={providers.slice(1)}>
-                <MultipleContextsProvider {...props} />
-            </NestedProviders> 
         )
     }
 
