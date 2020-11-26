eb4f9b04fc22438c654f19e209af3f08e44a5569
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-26 08:53:41 +0100

    Add login/logout button

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 import '../styles/HeaderBar.scss'
 
+import { AuthContext } from '../context/AuthContext'
 import { SearchBar } from './SearchBar'
 import { NavHeader } from './NavHeader'
 
 export const HeaderBar = () => {
+    const { user } = useContext(AuthContext)
+
     return (
         <header className='HeaderBar'>
             <NavHeader />
-            <SearchBar />
+
+            <div className='right-side'>
+                <SearchBar />
+
+                {user !== undefined ? user && user.id ? (
+                    <button className='login-btn'>Logout</button>
+                ) : (
+                    <Link className='login-btn' to='#'>Login</Link>
+                ): null}
+            </div>
         </header>
     )
 }
