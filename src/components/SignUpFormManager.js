5aa6229b4e5dfaef8e9bd55fc8e30c84911886f1
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-26 11:34:15 +0100

    create form manager to register form logic

---
+++ src/components/SignUpFormManager.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { SignUpForm } from './SignUpForm'
+
+export class SignUpFormManager extends React.Component {
+    state = {
+        data: {...this.props.initData}
+    }
+
+    updateFormData = (e) => {
+        const target = e.target
+        const stateClone = {...this.state}
+        stateClone.data[target.name] = target.value
+        this.setState(stateClone)
+    }
+
+    render = () => (
+        <SignUpForm 
+            className={this.state.className}
+            data={this.state.data}
+            updater={this.updateFormData} 
+        />
+    ) 
+}
+
+SignUpFormManager.propTypes = {
+    initData: PropTypes.shape({
+        username: PropTypes.string,
+        email: PropTypes.string,
+        password: PropTypes.string,
+        password2: PropTypes.string
+    }),
+    className: PropTypes.string
+}
+
+SignUpFormManager.defaultProps = {
+    initData: {
+        username: '',
+        email: '',
+        password: '',
+        password2: ''
+    },
+    className: ''
+}
