49a7b53ec6329c4232a1812d3093aff0b1149002
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-27 18:47:34 +0100

    Pass history object to Sign in form manager

--- src/pages/Register.page.js
+++ src/pages/Register.page.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import SignUpFormManager from '../components/SignUpFormManager'
+import { useHistory } from 'react-router-dom'
 
 import '../styles/RegisterPage.scss'
 
+import SignUpFormManager from '../components/SignUpFormManager'
+
 export const RegisterPage = () => {
+    const history = useHistory()
+
     return (
         <div className='RegisterPage page'>
-            <SignUpFormManager />
+            <SignUpFormManager history={history} />
         </div>
     )
 }
