99f0e641dfd4959693682f946d8dd5f344d72cdb
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-29 22:17:06 +0100

    Page to list all your follows

---
+++ src/pages/Subscription.page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class SubscriptionPage extends React.Component {
+    render = () => {
+        return (
+            <div className='SubscriptionPage page'>
+                SubscriptionPage
+            </div>
+        )
+    }
+}
