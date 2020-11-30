1b536b74055132172f3a5e87cc4df1b2f49daad9
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-30 18:18:13 +0100

    Get subscriptions list

--- src/pages/Feed.pages.js
+++ src/pages/Feed.pages.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { AuthencatedOnly } from '../components/AuthenticatedOnly'
+import SubscriptionPage from './Subscription.page'
 
 export const FeedPages = (props) => {
-    
+
     return (
         <Switch>
-            {/* <Route path={}></Route> */}
+            <Route path={`${props.match.path}/subscriptions`} component={SubscriptionPage} />
+            <Redirect exact from={`${props.match.path}`}  to={`${props.match.path}/subscriptions`} />
         </Switch>
     )
 }
--- src/pages/Subscription.page.js
+++ src/pages/Subscription.page.js
@@ -1,6 +1,40 @@
 import React from 'react'
 
+import { AuthContext } from '../context/AuthContext'
+import { ModelsContext } from '../context/ModelsContext'
+import { ClassWithMultipleContexts } from '../components/ClassWithMultipleContexts'
+
 export class SubscriptionPage extends React.Component {
+    state = {
+        data: [],
+        total: null,
+        
+    }
+
+    componentDidMount = () => {
+        this.getSubscriptions()
+    }
+
+    getSubscriptions = async () => {
+        if(this.context.AuthContext.isLoading) {
+            setTimeout(this.getSubscriptions, 200)
+            return
+        }
+
+        const req = await fetch(`${process.env.API_URL}/api/playlists/following`, {
+            credentials: 'include'
+        })
+        
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            this.setState({ data: data.artists, total: data.subscriptions_count })
+        } else {
+            console.error(await req.json())
+        }
+
+        this.context.ModelsContext.closeModel()
+    }
+
     render = () => {
         return (
             <div className='SubscriptionPage page'>
@@ -9,3 +43,5 @@ export class SubscriptionPage extends React.Component {
         )
     }
 }
+
+export default ClassWithMultipleContexts(SubscriptionPage, { AuthContext, ModelsContext })
