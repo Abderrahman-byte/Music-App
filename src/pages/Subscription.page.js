d453162d597e289e5b498959c9a1dae711f1b4e9
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-30 20:27:20 +0100

    Add Artists cards

--- src/pages/Subscription.page.js
+++ src/pages/Subscription.page.js
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { createRef } from 'react'
+
+import '../styles/SubscriptionsPage.scss'
 
 import { AuthContext } from '../context/AuthContext'
 import { ModelsContext } from '../context/ModelsContext'
 import { ClassWithMultipleContexts } from '../components/ClassWithMultipleContexts'
 import { PageHeader } from '../components/PageHeader'
+import { LoadingModel } from '../components/LoadingModel'
+import { ArtistCard } from '../components/ArtistCard'
 
-export class SubscriptionPage extends React.Component {
+export class SubscriptionsPage extends React.Component {
     state = {
         data: [],
         total: null,
         currentPage: 0,
-        itemsPerPage: 5
+        itemsPerPage: 5,
+        isLoading: true,
+        itemsPerLine: undefined,
+        containerRef: createRef(),
     }
 
     componentDidMount = () => {
         this.getSubscriptions()
+        this.getItemsPerLine()
+        window.addEventListener('resize', this.getItemsPerLine)
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener('resize', this.getItemsPerLine)
+    }
+
+    getItemsPerLine = () => {
+        const container = this.state.containerRef.current
+        const itemsPerLine = Math.floor(parseFloat(getComputedStyle(container).width) / 170)
+        this.setState({itemsPerLine: itemsPerLine})
     }
 
     getSubscriptions = async () => {
         if(this.context.AuthContext.isLoading) {
             setTimeout(this.getSubscriptions, 200)
             return
-        }
+        } 
+
+        if(this.state.total && this.state.data.length >= this.state.total) return
+
+        this.context.ModelsContext.openModel(<LoadingModel msg='Getting subscriptions' />)
+        this.setState({ isLoading : false })
 
         const nextPage = this.state.currentPage + 1
         const index = (nextPage - 1) * this.state.itemsPerPage
@@ -31,7 +55,12 @@ export class SubscriptionPage extends React.Component {
         
         if(req.status >= 200 && req.status < 300) {
             const data = await req.json()
-            this.setState({ data: data.artists, total: data.subscriptions_count })
+            this.setState({ 
+                data: [...this.state.data, ...data.artists], 
+                total: data.subscriptions_count, 
+                currentPage: nextPage ,
+                isLoading: false
+            })
         } else {
             console.error(await req.json())
         }
@@ -41,11 +70,22 @@ export class SubscriptionPage extends React.Component {
 
     render = () => {
         return (
-            <div className='SubscriptionPage page'>
+            <div className='SubscriptionsPage page'>
                 <PageHeader title='Subscriptions' />
+                <div className='cards-container' ref={this.state.containerRef}>
+                    {this.state.data.map(artist => <ArtistCard 
+                        key={artist.id} 
+                        data={artist} 
+                        itemsPerLine={this.state.itemsPerLine} 
+                    />)}
+                </div>
+                
+                {!this.state.isLoading && (!this.state.total || this.state.data.length < this.state.total) ? (
+                    <button className='add-more' onClick={this.getSubscriptions}>Load More</button>
+                ) : (null)}
             </div>
         )
     }
 }
 
-export default ClassWithMultipleContexts(SubscriptionPage, { AuthContext, ModelsContext })
+export default ClassWithMultipleContexts(SubscriptionsPage, { AuthContext, ModelsContext })
