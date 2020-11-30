662ab094aa1ac408d6d35a4cc093462740ef711a
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-11-30 17:10:11 +0100

    Add patname to be redirected to after login

--- src/components/AuthenticatedOnly.js
+++ src/components/AuthenticatedOnly.js
@@ -1,18 +1,24 @@
 import React, { useContext } from 'react'
-import { Redirect } from 'react-router-dom'
+import { Redirect, useLocation } from 'react-router-dom'
 
 import { AuthContext } from '../context/AuthContext'
 
 export const AuthencatedOnly = (Component) => {
     return (props) => {
         const { user } = useContext(AuthContext)
+        const location = useLocation()
 
         if(user === undefined) {
             return <></>
         } else if(user) {
             return <Component {...props} />
         } else {
-            return <Redirect to='/login' />
+            return <Redirect to={{
+                pathname: '/login',
+                state: {
+                    from : location.pathname
+                }
+            }} />
         }
     }
 }
