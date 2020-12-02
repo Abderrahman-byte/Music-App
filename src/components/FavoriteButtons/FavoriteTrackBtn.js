7fd45d36924023c43c61e1a508ec2755df4dccfc
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-02 20:12:07 +0100

    Check if track exists in favorite tracks list

--- src/components/FavoriteButtons/FavoriteTrackBtn.js
+++ src/components/FavoriteButtons/FavoriteTrackBtn.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import PropTypes from 'prop-types' 
 
 import '../../styles/FavoriteButtons.scss'
 
-export const FavoriteTracksBtn = () => {
+import { PlaylistsContext } from '../../context/PlaylistsContext'
+
+export const FavoriteTracksBtn = ({ id }) => {
+    const { getFavoriteTracks } = useContext(PlaylistsContext)
+    const [isInFavorite, setInFavorite] = useState(false)
+
+    const checkIfInFavorite = async () => {
+        const favoriteTracks = await getFavoriteTracks()
+        setInFavorite(favoriteTracks.some(track => track.id === id))
+    }
+
+    useEffect(() => {
+        checkIfInFavorite()    
+    }, [])
+
     return (
-        <button className='FavoriteButton active'>
+        <button className={`FavoriteButton ${isInFavorite ? 'active': ''}`}>
             <i className='fas fa-heart'></i>
         </button>
     )
+}
+
+FavoriteTracksBtn.propTypes = {
+    id: PropTypes.string.isRequired
 }
