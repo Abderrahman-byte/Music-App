1867a699232f55bab6cf6c2382932ec06b9e3230
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-02 11:46:08 +0100

    get playlists data as needed

--- src/context/PlaylistsContext.js
+++ src/context/PlaylistsContext.js
@@ -6,8 +6,40 @@ export const PlaylistsContext = createContext({})
 export class PlaylistsProvider extends React.Component {
     static contextType = AuthContext
 
-    state = {
-        
+    state = {}
+
+    initList = async (path, dataName, type) => {
+        if(this.context.isLoading) {
+            return new Promise((resolve, reject) => {
+                setTimeout(async () => {
+                    resolve(await this.initList(path, dataName, type))
+                }, 200)
+            })
+        }
+
+        const req = await fetch(`${process.env.API_URL}/api/playlists/${path}`, {
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json'}
+        })
+
+        if(req.status >= 200 && req.status < 300) {
+            const data = await req.json()
+            const newState = {}
+            newState[dataName] = [...(data[type] || data.data || [])]
+            this.setState(newState)
+            return [...(data[type] || data.data || [])]
+        } else {
+            console.error(await req.json())
+            return []
+        }
+    }
+
+    getFavoriteTracks = async () => {
+        if(this.state.favoriteTracks === undefined) {
+            return await this.initList('favorite/tracks', 'favoriteTracks', 'tracks')
+        } else {
+            return this.state?.favoriteTracks
+        }
     }
 
     render = () => {
