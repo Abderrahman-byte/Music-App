b456760626c00f8397fdb3dc33dac25eb0d51802
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-02 12:06:10 +0100

    export context api functions

--- src/context/PlaylistsContext.js
+++ src/context/PlaylistsContext.js
@@ -1,4 +1,5 @@
 import React, { createContext } from 'react'
+
 import { AuthContext } from './AuthContext'
 
 export const PlaylistsContext = createContext({})
@@ -38,13 +39,53 @@ export class PlaylistsProvider extends React.Component {
         if(this.state.favoriteTracks === undefined) {
             return await this.initList('favorite/tracks', 'favoriteTracks', 'tracks')
         } else {
-            return this.state?.favoriteTracks
+            return this.state?.favoriteTracks || []
+        }
+    }
+
+    getFavoriteAlbums = async () => {
+        if(this.state.favoriteAlbums === undefined) {
+            return await this.initList('favorite/albums', 'favoriteAlbums', 'albums')
+        } else {
+            return this.state?.favoriteAlbums || []
+        }
+    }
+
+    getFavoriteArtists = async () => {
+        if(this.state.favoriteArtists === undefined) {
+            return await this.initList('favorite/artists', 'favoriteArtists', 'artists')
+        } else {
+            return this.state?.favoriteArtists || []
+        }
+    }
+
+    getFavoritePlaylists = async () => {
+        if(this.state.favoritePlaylists === undefined) {
+            return await this.initList('favorite/playlists', 'favoritePlaylists', 'playlists')
+        } else {
+            return this.state?.favoritePlaylists || []
+        }
+    }
+
+    getPlaylists = async () => {
+        if(this.state.playlists === undefined) {
+            return await this.initList('', 'playlists', 'playlists')
+        } else {
+            return this.state?.playlists || []
         }
     }
 
     render = () => {
+        const contextData = {
+            getFavoriteAlbums: this.getFavoriteAlbums,
+            getFavoriteArtists: this.getFavoriteArtists,
+            getFavoritePlaylists: this.getFavoritePlaylists,
+            getFavoriteTracks: this.getFavoriteTracks,
+            getPlaylists: this.getPlaylists
+        }
+
         return (
-            <PlaylistsContext.Provider value={{}}>
+            <PlaylistsContext.Provider value={contextData}>
                 {this.props.children}
             </PlaylistsContext.Provider>
         )    
