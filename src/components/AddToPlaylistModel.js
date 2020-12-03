cce5a6f19369e5bebaea00bd0e19ea3804f0567b
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-03 19:47:42 +0100

    Display playlists

--- src/components/AddToPlaylistModel.js
+++ src/components/AddToPlaylistModel.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import '../styles/AddToPlaylistModel.scss'
 
-export const AddToPlaylistModel = () => {
+const PlaylistCheckItem = ({ playlist, trackId }) => {
+    return (
+        <div className='PlaylistCheckItem'>
+            <h6>{playlist.title}</h6>
+        </div>
+    )
+} 
+
+export const AddToPlaylistModel = ({ playlists, id, data }) => {
+
+    useEffect(() => {
+        console.log(playlists)
+        console.log(id)
+        console.log(data)
+    }, [])
+
     return (
         <div className='model AddToPlaylistModel'>
-            this playlist models
+            {playlists.map(playlist => <PlaylistCheckItem key={playlist.id} playlist={playlist} trackId={id} />)}
         </div>
     )
 }
