a418d54a88d170fc767a6649478b572e719568dc
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-03 09:52:44 +0100

    Check if user is login before checking if track exists is logged in

--- src/components/FavoriteButtons/FavoriteTrackBtn.js
+++ src/components/FavoriteButtons/FavoriteTrackBtn.js
@@ -5,17 +5,33 @@ import '../../styles/FavoriteButtons.scss'
 
 import { PlaylistsContext } from '../../context/PlaylistsContext'
 import { getCookie } from '../../utils/http'
+import { AuthContext } from '../../context/AuthContext'
+import { ModelsContext } from '../../context/ModelsContext'
+import LoginFormManager from '../LoginFormManager'
 
 export const FavoriteTracksBtn = ({ id, data }) => {
     const { getFavoriteTracks, removeFromFavoriteTracks, addToFavoriteTracks } = useContext(PlaylistsContext)
+    const { user } = useContext(AuthContext)
+    const { openModel } = useContext(ModelsContext)
+
     const [isInFavorite, setInFavorite] = useState(false)
 
     const checkIfInFavorite = async () => {
+        if(!user) {
+            setInFavorite(false)
+            return
+        }
+
         const favoriteTracks = await getFavoriteTracks()
         setInFavorite(favoriteTracks.some(track => track.id === id))
     }
 
     const toggleFromFavorite = () => {
+        if(!user) {
+            openModel(<LoginFormManager isModel className='model' />, true)
+            return
+        }
+
         fetch(`${process.env.API_URL}/api/playlists/favorite/tracks`, {
             credentials: 'include',
             method: isInFavorite ? 'DELETE' : 'POST',
@@ -33,7 +49,7 @@ export const FavoriteTracksBtn = ({ id, data }) => {
 
     useEffect(() => {
         checkIfInFavorite()
-    }, [])
+    }, [user])
 
     return (
         <button onClick={toggleFromFavorite} className={`FavoriteButton ${isInFavorite ? 'active': ''}`}>
