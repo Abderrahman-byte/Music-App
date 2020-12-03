ad4b8a5600cf0ca71b91717aa386e270ac552cec
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-03 09:17:23 +0100

    Create button that add tracks to playlists

---
+++ src/components/AddToPlaylistBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../styles/AddToPlaylistBtn.scss'
+
+export const AddToPlaylistBtn = () => {
+    return (
+        <button className='AddToPlaylistBtn'>
+            <i className='fas fa-plus'></i>
+        </button>
+    )
+}
