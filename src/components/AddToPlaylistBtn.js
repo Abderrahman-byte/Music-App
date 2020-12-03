08b9168bce1a62bbcb5f8c4381ffaadd12908434
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-03 10:58:59 +0100

    Pop up playlists model on button clicked

--- src/components/AddToPlaylistBtn.js
+++ src/components/AddToPlaylistBtn.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import '../styles/AddToPlaylistBtn.scss'
 
-export const AddToPlaylistBtn = () => {
+import { ModelsContext } from '../context/ModelsContext'
+import { AuthContext } from '../context/AuthContext'
+import { AddToPlaylistModel } from './AddToPlaylistModel'
+
+export const AddToPlaylistBtn = ({ id, data }) => {
+    const { openModel } = useContext(ModelsContext)
+    const { user } = useContext(AuthContext)
+
+    const handleClicked = () => {
+        if(user) openModel(<AddToPlaylistModel />, true)
+    }
+
     return (
-        <button className='AddToPlaylistBtn'>
+        <button className='AddToPlaylistBtn' onClick={handleClicked}>
             <i className='fas fa-plus'></i>
         </button>
     )
