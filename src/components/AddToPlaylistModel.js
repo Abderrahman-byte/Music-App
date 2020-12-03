83e56740fe428ac9419e08b9a20f420e6139f25c
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-03 10:54:17 +0100

    Create model that display playlist and if track in playlists

---
+++ src/components/AddToPlaylistModel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../styles/AddToPlaylistModel.scss'
+
+export const AddToPlaylistModel = () => {
+    return (
+        <div className='model AddToPlaylistModel'>
+            this playlist models
+        </div>
+    )
+}
