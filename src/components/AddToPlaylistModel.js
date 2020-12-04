c581d690462c6491cadc8e2c12c3e4f945451fc7
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-04 17:58:11 +0100

    Pass toggle callback to each playlist item

--- src/components/AddToPlaylistModel.js
+++ src/components/AddToPlaylistModel.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react'
 
 import '../styles/AddToPlaylistModel.scss'
 
-const PlaylistCheckItem = ({ playlist, trackId }) => {
+const PlaylistCheckItem = ({ playlist, trackId, toggleCallback }) => {
     const [isInPlaylist, setInPlaylist] = useState(playlist.tracks.some(track => track.id === trackId) || false)
 
     const handleChange = (e) => {
         setInPlaylist(e.target.checked)
+        toggleCallback(playlist.id, e.target.checked)
     }
 
     return (
         <div className='PlaylistCheckItem'>
-            {/* {playlist.tracks.some(track => track.id === trackId)} */}
             {playlist.is_public ? (
                 <i className="fas fa-globe-africa"></i>
             ): (
-                <i className='fas fa-lock'></i>        
+                <i className='fas fa-lock'></i>
             )}
             
             <h6 className='title'>{playlist.title}</h6>
@@ -28,7 +28,7 @@ const PlaylistCheckItem = ({ playlist, trackId }) => {
     )
 } 
 
-export const AddToPlaylistModel = ({ playlists, id, data }) => {
+export const AddToPlaylistModel = ({ playlists, id, toggleCallback }) => {
 
     return (
         <div className='model AddToPlaylistModel'>
@@ -37,6 +37,7 @@ export const AddToPlaylistModel = ({ playlists, id, data }) => {
                     key={playlist.id} 
                     playlist={playlist} 
                     trackId={id} 
+                    toggleCallback={toggleCallback}
                 />)}
             </div>
         </div>
