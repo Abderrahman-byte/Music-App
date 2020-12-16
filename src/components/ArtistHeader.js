9d52a59ecbd373f38a72d0a038c232aa674a3c83
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-12-16 21:39:45 +0100

    Add Favorite button to artist header

--- src/components/ArtistHeader.js
+++ src/components/ArtistHeader.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import '../styles/ArtistHeader.scss'
 
 import { FollowBtn } from './FollowBtn'
+import { FavoriteArtistsBtn } from './FavoriteButtons/FavoriteArtistBtn'
 
 export const ArtistHeader = ({id, name, picture, nb_albums, nb_tracks, nb_followers}) => {
     const [nb_follow, setFollowNb] = useState(nb_followers || 0)
@@ -24,9 +25,7 @@ export const ArtistHeader = ({id, name, picture, nb_albums, nb_tracks, nb_follow
 
                 <div className='info-row'>
                     <FollowBtn id={id} callback={updateFollows} />
-                    <button className='fav-btn'>
-                        <i className='fas fa-heart'></i>
-                    </button>
+                    <FavoriteArtistsBtn id={id} data={{id, name, nb_albums, nb_tracks, nb_followers}} />
                 </div>
             </div>
         </div>
