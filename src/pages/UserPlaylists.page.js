c9c6c4fdf64307b3db9804e190eed7394595789b
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-01-19 18:13:44 +0100

    Get user playlists then close model if auth data is loaded

--- src/pages/UserPlaylists.page.js
+++ src/pages/UserPlaylists.page.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { ClassWithMultipleContexts } from '../components/ClassWithMultipleContexts'
+
+import { ModelsContext } from '../context/ModelsContext'
+import { AuthContext } from '../context/AuthContext'
+import { PlaylistsContext } from '../context/PlaylistsContext'
+import { PageHeader } from '../components/PageHeader'
 
 export class UserPlaylistsPage extends React.Component {
+    componentDidMount = () => {
+        this.getPlaylistsData()
+    }
+
+    closeModelOnAuthLoaded = () => {
+        if(this.context.AuthContext.isLoading)
+            setTimeout(this.closeModelOnAuthLoaded, 300)
+        else this.context.ModelsContext.closeModel() 
+    }
+
+    getPlaylistsData = async () => {
+        const playlists = await this.context.PlaylistsContext.getPlaylists()
+        this.closeModelOnAuthLoaded()
+    }
+
     render = () => {
         return (
-            <div className='UserPlaylistsPage'>
-                This Playlists Page
+            <div className='UserPlaylistsPage page'>
+                <PageHeader title='User Playlists' />
             </div>
         )
     }
 }
+
+export default ClassWithMultipleContexts(UserPlaylistsPage, { ModelsContext, AuthContext, PlaylistsContext })
