cfa39c4f94864b270e3a0709123bf73c1a12eb4d
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-01-19 18:26:07 +0100

    Import default playlists page

--- src/pages/Feed.pages.js
+++ src/pages/Feed.pages.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 import { AuthencatedOnly } from '../components/AuthenticatedOnly'
-import { UserPlaylistsPage } from './UserPlaylists.page'
+import PlaylistsPage from './UserPlaylists.page'
 import SubscriptionPage from './Subscription.page'
 
 export const FeedPages = (props) => {
@@ -10,7 +10,7 @@ export const FeedPages = (props) => {
     return (
         <Switch>
             <Route path={`${props.match.path}/subscriptions`} component={SubscriptionPage} />
-            <Route path={`${props.match.path}/playlists`} component={UserPlaylistsPage} />
+            <Route path={`${props.match.path}/playlists`} component={PlaylistsPage} />
             <Redirect exact from={`${props.match.path}`}  to={`${props.match.path}/subscriptions`} />
         </Switch>
     )
