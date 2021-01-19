f5130515db1ba29bb44f45124f6e0840fdab753f
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-01-19 19:03:54 +0100

    Create Playlist data Row

---
+++ src/components/PlaylistRow.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import '../styles/TableOfPlaylists.scss'
+
+export const PlaylistRow = () => {
+    return (
+        <tr className='PlaylistRow'>
+            
+        </tr>
+    )
+}
+
+PlaylistRow.propTypes = {
+    data: PropTypes.object.isRequired
+}
