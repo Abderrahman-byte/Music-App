6061ff5536c8abdd91ec8008a837599732803204
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-01-19 20:41:13 +0100

    Display playlist data

--- src/components/PlaylistRow.js
+++ src/components/PlaylistRow.js
@@ -3,14 +3,34 @@ import PropTypes from 'prop-types'
 
 import '../styles/TableOfPlaylists.scss'
 
-export const PlaylistRow = () => {
+export const PlaylistRow = ({index, data}) => {
+    console.log(data)
     return (
         <tr className='PlaylistRow'>
-            
+            <td>{data.is_public ? (
+                <i className='fas fa-globe'></i>
+            ) : (
+                <i className='fa fa-lock'></i>
+            )}</td>
+            <td>{data.title}</td>
+            <td>@{data.author.username}</td>
+            <td>{data.tracks_count}</td>
+
+            <td>
+                <i className='fas fa-heart'></i>
+            </td>
+
+            <td>
+                <i className='fas fa-plus'></i>
+            </td>
         </tr>
     )
 }
 
 PlaylistRow.propTypes = {
-    data: PropTypes.object.isRequired
+    data: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired
+    }).isRequired,
+    index: PropTypes.number.isRequired
 }
