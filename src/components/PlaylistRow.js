2a4b847b1883e2d338604cd13fef29df4a315790
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-01-22 11:38:27 +0100

    Add data and styling to playlist row

--- src/components/PlaylistRow.js
+++ src/components/PlaylistRow.js
@@ -1,27 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-import '../styles/TableOfPlaylists.scss'
+import '../styles/PlaylistRow.scss'
 
-export const PlaylistRow = ({index, data}) => {
+export const PlaylistRow = ({withAuthor, data}) => {
     console.log(data)
     return (
         <tr className='PlaylistRow'>
-            <td>{data.is_public ? (
+            <td className='text-start'>{data.is_public ? (
                 <i className='fas fa-globe'></i>
             ) : (
                 <i className='fa fa-lock'></i>
             )}</td>
-            <td>{data.title}</td>
-            <td>@{data.author.username}</td>
-            <td>{data.tracks_count}</td>
 
             <td>
-                <i className='fas fa-heart'></i>
+                <Link className='playlist-title' to={`/playlist/${data.id}`}>{data.title}</Link>
             </td>
 
-            <td>
-                <i className='fas fa-plus'></i>
+            {withAuthor ? (
+                <td>@{data.author.username}</td>
+            ) : null}
+
+            <td>{data.tracks_count}</td>
+            <td>{new Date(data.created_date || 0).toLocaleString()} </td>
+
+            <td className='col-btn'>
+                <button className='btn btn-edit'>
+                    <i className='fas fa-pen'></i>
+                </button>
+            </td>
+
+            <td className='col-btn'>
+                <button className='btn btn-delete'>
+                    <i className='fas fa-trash'></i>
+                </button>
             </td>
         </tr>
     )
@@ -32,5 +45,5 @@ PlaylistRow.propTypes = {
         id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired
     }).isRequired,
-    index: PropTypes.number.isRequired
+    withAuthor: PropTypes.bool
 }
