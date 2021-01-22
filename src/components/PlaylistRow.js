186c86026e9bc6412828f9b771523ceed34c463a
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-01-22 12:07:07 +0100

    Delete playlist after delete-btn has been clicked

--- src/components/PlaylistRow.js
+++ src/components/PlaylistRow.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import '../styles/PlaylistRow.scss'
 
-export const PlaylistRow = ({withAuthor, data}) => {
-    console.log(data)
+import { getCookie } from '../utils/http'
+import { ModelsContext } from '../context/ModelsContext'
+import { ConfirmModel } from './ConfirmModel'
+
+export const PlaylistRow = ({withAuthor, data, deletePlaylist}) => {
+    const { openModel, closeModel } = useContext(ModelsContext)
+
+    const editPlaylist = () => {
+        console.log(`%c Edit playlist ${data.id}`, "color: royalblue;")
+    }
+
+    const deletePlaylistConfimed = () => {
+        deletePlaylist(data.id)
+    
+        fetch(`${process.env.API_URL}/api/playlists/list/${data.id}`, {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                'X-CSRFTOKEN': getCookie('csrftoken')
+            }
+        })
+
+        closeModel()
+        console.log(`%c Playlist ${data.id} deleted`, "color: red;")
+    }
+
+    const deletePlaylistBtn = () => {
+        openModel(<ConfirmModel 
+            callback={deletePlaylistConfimed} 
+            action='delete' 
+            msg={`Do you really wan to delete playlist "${data.title}" ?`}  
+        />, true)
+    }
+
     return (
         <tr className='PlaylistRow'>
             <td className='text-start'>{data.is_public ? (
@@ -26,13 +58,13 @@ export const PlaylistRow = ({withAuthor, data}) => {
             <td>{new Date(data.created_date || 0).toLocaleString()} </td>
 
             <td className='col-btn'>
-                <button className='btn btn-edit'>
+                <button className='btn btn-edit' onClick={editPlaylist}>
                     <i className='fas fa-pen'></i>
                 </button>
             </td>
 
             <td className='col-btn'>
-                <button className='btn btn-delete'>
+                <button className='btn btn-delete' onClick={deletePlaylistBtn}>
                     <i className='fas fa-trash'></i>
                 </button>
             </td>
@@ -45,5 +77,6 @@ PlaylistRow.propTypes = {
         id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired
     }).isRequired,
-    withAuthor: PropTypes.bool
+    withAuthor: PropTypes.bool,
+    deletePlaylist: PropTypes.func.isRequired
 }
