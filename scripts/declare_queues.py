a8c07fe76c4273751754c85a0c8d6cc55dc280ba
abderrahmane <el.abderrahman00@gmail.com>
2021-01-26 19:50:41 +0100

    Connect to server by connect function

--- scripts/declare_queues.py
+++ scripts/declare_queues.py
@@ -22,14 +22,18 @@ def checkConf(conf) :
         print('Vhost not provided')
         sys.exit(1)
 
-def run () :
+def connect() :
     conf = settings.RABBITMQ
     checkConf(conf)
     
     credentials = pika.PlainCredentials(conf.get('user'), conf.get('password'))
     parameters = pika.ConnectionParameters(conf.get('host'), conf.get('port'), conf.get('vhost'), credentials)
     connection = pika.BlockingConnection(parameters)
-    
+
+    return connection
+
+def run () :
+    connection = connect()
     channel = connection.channel()
 
     for queue in queues :
