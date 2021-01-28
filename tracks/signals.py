e2544f5e425585662eeb814bddb0f6a0d558c858
abderrahmane <el.abderrahman00@gmail.com>
2021-01-28 16:35:16 +0100

    Change signals to publish to queues instead of sending getting data

--- tracks/signals.py
+++ tracks/signals.py
@@ -1,115 +1,50 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
 import requests, json, time
 from datetime import datetime
-import logging
+import logging, pika
 
 from .models import Artist, Album, Genre, Track
 
-def getAlbums(instance, nb) :
-    url = f'https://api.deezer.com/artist/{instance.deezer_id}/albums?limit={nb}'
-    try :
-        req = requests.get(url)
-        content = json.loads(req.content.decode('utf-8'))
-    except Exception as ex :
-        logging.getLogger('errors').error(f'Couldnt get albums of "{instance.name}" because : ' + ex.__str__())
-        return
-
-    if content.get('data') is None or  type(content.get('data')) != list or len(content.get('data')) <= 0 or content.get('total') == 0 :
-        return
-
-    for album in content.get('data') :
-        title = album.get('title')
-        deezer_id = album.get('id')
-        release = album.get('release_date')
-
-        if release is not None : release = datetime.strptime(release, '%Y-%m-%d')
-
-        try :
-            album = Album.objects.get(deezer_id=deezer_id)
-            logging.getLogger('warnings').warning(f'album "{title}" with id "{deezer_id}" already exists')
-        except Album.DoesNotExist :
-            album = Album(deezer_id=deezer_id, title=title, release_date=release, artist=instance)
-
-        try :
-            album.save()
-        except Exception as ex :
-            logging.getLogger('errors').error(f'Album "{album.title}" coundnt be saved because ' + ex.__str__()) 
-
-        time.sleep(0.5)
-
-def getAlbumDetails(instance) :
-    url = f'https://api.deezer.com/album/{instance.deezer_id}'
-    try :
-        req = requests.get(url)
-        content = json.loads(req.content.decode('utf-8'))
-    except Exception as ex :
-        logging.getLogger('errors').error(f'Couldnt get details of album "{instance.title}" because : ' + ex.__str__())
-        return
-
-    genres = content.get('genres', {}).get('data')
-    tracks = content.get('tracks', {}).get('data')
-
-    instance.cover_big = content.get('cover_big', instance.cover_big)
-    instance.cover_medium = content.get('cover_medium', instance.cover_medium)
-    instance.cover_small = content.get('cover_small', instance.cover_small)
-    instance.cover_xl = content.get('cover_xl', instance.cover_xl)
-
-    if genres is not None and type(genres) == list and len(genres) >= 0:
-        for genre in genres :
-            # Get Or Create genre
-            if type(genre) != dict or genre.get('name') is None or genre.get('id') is None  :
-                continue
-            g, created = Genre.objects.get_or_create(deezer_id=genre.get('id'), name=genre.get('name'), picture=genre.get('picture'))
-            instance.genres.add(g)
-
-    instance.save()
-
-    if tracks is not None and type(tracks) == list and len(tracks) >= 0:
-        for track in tracks :
-            # Create Track
-            if type(track) != dict or track.get('title') is None or track.get('id') is None  :
-                return
-                
-            try :
-                t = Track(
-                    deezer_id=track.get('id'), 
-                    album=instance, 
-                    preview=track.get('preview'), 
-                    title=track.get('title'),
-                    rank=track.get('rank')
-                )
-                t.save()
-            except Exception as ex :
-                logging.getLogger('errors').error(f'Track "{t.title}" couldnt because ' + ex.__str__())
-
-def getArtistsDetails(instance) :
-    url = f'https://api.deezer.com/artist/{instance.deezer_id}'
-
-    try :
-        req = requests.get(url)
-        content = json.loads(req.content.decode('utf-8'))
-    except Exception as ex :
-        logging.getLogger('errors').error(f'Couldnt Artist details "{instance.name}" because : ' + ex.__str__())
-        return
+def rabbitmq_connection() :
+    conf = settings.RABBITMQ
+    credentials = pika.PlainCredentials(conf.get('user'), conf.get('password'))
+    parameters = pika.ConnectionParameters(conf.get('host'), conf.get('port'), conf.get('vhost'), credentials)
+    connection = pika.BlockingConnection(parameters)
     
-    instance.picture = content.get('picture', instance.picture)
-    instance.picture_small = content.get('picture_small', instance.picture_small)
-    instance.picture_medium = content.get('picture_medium', instance.picture_medium)
-    instance.picture_big = content.get('picture_big', instance.picture_big)
-    instance.picture_xl = content.get('picture_xl', instance.picture_xl)
-    instance.save()
+    return connection
 
-    nb_album = content.get('nb_album')
-    getAlbums(instance, nb_album)
+def publish_to_queue(queue, body) :
+    connection = rabbitmq_connection()
+    channel = connection.channel()
+    channel.basic_publish(exchange='', routing_key=queue, body=body, 
+        properties=pika.BasicProperties(delivery_mode=2)
+    )
+    connection.close()
+
+def AlbumCreated(sender, instance, created, *args, **kwargs) :
+    print('AlbumCreated()')
+
+    if created :
+        print('new albums has been created')
+        try :
+            logging.getLogger('debuging').debug(f'Album created {instance.title} with id {instance.deezer_id}')
+            publish_to_queue('album_tracks', str(instance.deezer_id))
+            logging.getLogger('debuging').debug(f'Album {instance.deezer_id} added to queue "album_tracks"')
+        except Exception as ex :
+            logging.getLogger('errors').error(f'Album created signal : {ex.__str__()}')
 
 @receiver(post_save, sender=Artist)
-def artistCreated(sender, instance, created, *args, **kwargs) :
-    if created :
-        getArtistsDetails(instance)
+def ArtistCreated(sender, instance, created, *args, **kwargs) :
+    print('ArtistCreated()')
 
-@receiver(post_save, sender=Album)
-def albumCreated(sender, instance, created, *args, **kwargs) :
     if created :
-        getAlbumDetails(instance)
+        print('new artist has been created')
+        try :
+            logging.getLogger('debuging').debug(f'Artist created "{instance.name}" with id {instance.deezer_id}')
+            publish_to_queue('artist_albums', str(instance.deezer_id))
+            logging.getLogger('debuging').debug(f'Artist {instance.deezer_id} added to queue "artist_albums"')
+        except Exception as ex :
+            logging.getLogger('errors').error(f'Artist created signal : {ex.__str__()}')
