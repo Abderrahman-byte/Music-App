1c322bca27e9407690afda54e4406c911167e5e5
abderrahmane <el.abderrahman00@gmail.com>
2021-01-30 22:16:40 +0100

    Catch error of not publishing to queue

--- tracks/signals.py
+++ tracks/signals.py
@@ -22,18 +22,27 @@ def publish_to_queue(queue, body) :
     channel.basic_publish(exchange='', routing_key=queue, body=body, 
         properties=pika.BasicProperties(delivery_mode=2)
     )
+    # channel.basic_publish(exchange='', routing_key=queue, body=body)
     connection.close()
 
 @receiver(post_save, sender=Album)
 def AlbumCreated(sender, instance, created, *args, **kwargs) :
     if created :
+        logging.getLogger('debuging').debug(f'Album created "{instance.title}" with id "{instance.deezer_id}"')
         try :
-            logging.getLogger('debuging').debug(f'Album created {instance.title} with id {instance.deezer_id}')
             publish_to_queue('album_tracks', str(instance.deezer_id))
-            publish_to_queue('album_genres', str(instance.deezer_id))
             logging.getLogger('debuging').debug(f'Album {instance.deezer_id} added to queue "album_tracks"')
+            time.sleep(1)
         except Exception as ex :
-            logging.getLogger('errors').error(f'Album created signal : {ex.__str__()}')
+            logging.getLogger('errors').error(f'Could not pulish to "album_tracks" : {ex.__str__()}')
+
+        try :
+            publish_to_queue('album_genres', str(instance.deezer_id))
+            logging.getLogger('debuging').debug(f'Album {instance.deezer_id} added to queue "album_genres"')
+            time.sleep(1)
+        except Exception as ex :
+            logging.getLogger('errors').error(f'Could not pulish to "album_genres" : {ex.__str__()}')
+
 
 @receiver(post_save, sender=Artist)
 def ArtistCreated(sender, instance, created, *args, **kwargs) :
@@ -43,4 +52,4 @@ def ArtistCreated(sender, instance, created, *args, **kwargs) :
             publish_to_queue('artist_albums', str(instance.deezer_id))
             logging.getLogger('debuging').debug(f'Artist {instance.deezer_id} added to queue "artist_albums"')
         except Exception as ex :
-            logging.getLogger('errors').error(f'Artist created signal : {ex.__str__()}')
+            logging.getLogger('errors').error(f'Could not pulish to "artist_albums" : {ex.__str__()}')
