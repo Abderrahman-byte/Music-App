9759e82174830216d87bc7165b402410a4154070
abderrahmane <el.abderrahman00@gmail.com>
2021-02-04 16:17:54 +0100

    Requeue all existed artists to double check for there albums

---
+++ scripts/requeue_all_artists.py
@@ -0,0 +1,36 @@
+import time, sys
+
+from tracks.models import Artist
+from .main import connect_to_broker
+from .get_artist_albums import sleepWithHeartbeats
+
+
+def publish(connection, ch, queue, body) :
+    try :
+        ch.basic_publish(exchange='', routing_key=queue, body=body)
+    except pika.exceptions.UnroutableError :
+        sleepWithHeartbeats(connection, 5)
+        publish(connection, ch, queue, body)
+        
+
+def run() :
+    try :
+        time_start = time.time()
+        artists_ids = [str(artist.deezer_id) for artist in Artist.objects.all()]
+        print(f'[*] fetched {len(artists_ids)} artist in {round(time.time() - time_start, 2)} s')
+    
+        connection = connect_to_broker()
+        channel = connection.channel()
+        print('[*] Connection with rabbitmq opened')
+        channel.confirm_delivery()
+
+        for count, artist in enumerate(artists_ids):
+            if count % 50 == 0 :
+                print(f'[*] delivered {count} in {round(time.time() - time_start, 2)} s')
+                sleepWithHeartbeats(connection, 3)
+
+            publish(connection, channel, 'artist_albums', artist)
+
+    except KeyboardInterrupt :
+        connection.close()
+        sys.exit(0)
